797a2ad6ee70ae4d439daf35cb47a510d3045ee2
nxelia99 <nxelia99@gmail.com>
2024-07-16 19:16:18 +0200

    2nd commit

---
+++ demo/src/main/java/com/example/demo/Challenge.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.example.demo;
+
+
+class Challenge {
+    private Long id;
+    private String month;
+    private String description;
+
+    public Challenge(String description, Long id, String month) {
+        this.description = description;
+        this.id = id;
+        this.month = month;
+    }
+
+
+
+}
